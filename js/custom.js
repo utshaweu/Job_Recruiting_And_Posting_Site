ed7c8d50224c5a357bd0e138ebfd20bd805e7756
utshaweu <mahmudsajib95@gmail.com>
2020-06-28 11:56:44 +0600

    home page completed

--- js/custom.js
+++ js/custom.js
@@ -24,5 +24,45 @@ $(function(){
     }
   });	
 
+  //Testimonial Slider
+  $('.stories-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    speed:1000,
+    autoplaySpeed: 2500,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          centerMode:false,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+      ]
+  });
+
+  //Counter Up Js
+  $('.counter').counterUp({
+    delay: 10,
+    time: 2000,
+  });
 
 });
